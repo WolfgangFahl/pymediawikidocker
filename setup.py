76fa6b90216c954e4c09b56a9a8829bfd9eaaecf
Wolfgang Fahl <wf@bitplan.com>
2021-06-24 17:37:20 +0200

    disable strict hostkey checking to fix #13

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pymediawikidocker',
-    version='0.0.22',
+    version='0.0.23',
 
     packages=['mwdocker',],
     
