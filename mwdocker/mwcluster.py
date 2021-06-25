a5ec801b5a0de14ec900f4b5221bb73ccb837d5f
Wolfgang Fahl <wf@bitplan.com>
2021-06-25 08:45:52 +0200

    removes MagicNoCache from defaultVersions since it's version 1.27 needs the old "require_once" approach to fix #14

--- mwdocker/mwcluster.py
+++ mwdocker/mwcluster.py
@@ -13,7 +13,7 @@ class MediaWikiCluster(object):
     a cluster of mediawiki docker Applications
     '''
     defaultVersions=["1.27.7","1.31.14","1.35.2","1.36.0"]
-    defaultExtensionNameList=["Admin Links","Header Tabs","MagicNoCache","SyntaxHighlight","Variables"]
+    defaultExtensionNameList=["Admin Links","Header Tabs","SyntaxHighlight","Variables"]
     defaultUser="Sysop"
     defaultPassword="sysop-1234!"
 
