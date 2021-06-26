b8fdca7715f1371c33752a2b492061755f202681
Wolfgang Fahl <wf@bitplan.com>
2021-06-26 20:31:10 +0200

    prepares 0.0.33 release with ParserFunctions extension

--- mwdocker/mwcluster.py
+++ mwdocker/mwcluster.py
@@ -149,7 +149,7 @@ class MediaWikiCluster(object):
         mwApp=DockerApplication(user=self.user,password=self.password,version=version,extensionMap=self.extensionMap,wikiId=wikiId,mariaDBVersion=self.mariaDBVersion,smwVersion=self.smwVersion,port=port,sqlPort=sqlPort,mySQLRootPassword=self.mySQLRootPassword,debug=True)
         return mwApp
 
-__version__ = "0.0.32"
+__version__ = "0.0.33"
 __date__ = '2021-06-21'
 __updated__ = '2021-06-26'
 DEBUG=False
