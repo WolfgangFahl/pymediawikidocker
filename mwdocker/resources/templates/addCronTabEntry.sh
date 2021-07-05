d2153007bebd141b6835d0b92946cdbc5154a908
Wolfgang Fahl <wf@bitplan.com>
2021-07-05 20:55:38 +0200

    fixes runLog availability

--- mwdocker/resources/templates/addCronTabEntry.sh
+++ mwdocker/resources/templates/addCronTabEntry.sh
@@ -2,6 +2,8 @@
 # WF 2021-07-05
 # add crontab entry
 # run startRunJobs.sh every minute
+# prepare logging
 mkdir -p /var/log/mediawiki
+touch /var/log/mediawiki/runJobs.log
 (crontab -l 2>/dev/null; echo "*/1 * * * * /root/startRunJobs.sh") | crontab -
 service cron start
