1bf57157dbb00387c41dcf98d81bd0c03e0f590e
Wolfgang Fahl <wf@bitplan.com>
2021-10-11 14:39:19 +0200

    upgrades minor versions

--- mwdocker/mwcluster.py
+++ mwdocker/mwcluster.py
@@ -12,7 +12,7 @@ class MediaWikiCluster(object):
     '''
     a cluster of mediawiki docker Applications
     '''
-    defaultVersions=["1.27.7","1.31.15","1.35.3","1.36.1"]
+    defaultVersions=["1.27.7","1.31.16","1.35.4","1.36.2"]
     defaultExtensionNameList=["Admin Links","Header Tabs","SyntaxHighlight","Variables"]
     defaultUser="Sysop"
     defaultPassword="sysop-1234!"
