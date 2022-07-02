d99d4fad06b34a486789a2a58831c370e1f81d2e
Wolfgang Fahl <wf@bitplan.com>
2022-07-02 18:23:04 +0200

    prepares 0.3.2 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pymediawikidocker',
-    version='0.3.1',
+    version='0.3.2',
 
     packages=['mwdocker',],
     
