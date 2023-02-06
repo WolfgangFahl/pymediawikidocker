9ce9950c5a14763b9f396c4989646d696c7bdd5b
Wolfgang Fahl <wf@bitplan.com>
2023-02-06 08:18:58 +0100

    adds file stubs for Mediawiki 1.39

--- mwdocker/resources/templates/addCronTabEntry.sh
+++ mwdocker/resources/templates/addCronTabEntry.sh
@@ -5,5 +5,7 @@
 # prepare logging
 mkdir -p /var/log/mediawiki
 touch /var/log/mediawiki/runJobs.log
+# add an entry for the crontab
 (crontab -l 2>/dev/null; echo "*/1 * * * * /root/startRunJobs.sh") | crontab -
+# start the cron service
 service cron start
