9c6174b8877dea4af1da3048a77237e3749a7a93
Wolfgang Fahl <wf@bitplan.com>
2023-02-07 07:09:03 +0100

    fixes icon handling

--- mwdocker/resources/templates/mwLocalSettings139.php
+++ mwdocker/resources/templates/mwLocalSettings139.php
@@ -38,8 +38,8 @@ $wgResourceBasePath = $wgScriptPath;
 ## The URL paths to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
 $wgLogos = [
-	'1x' => "$wgResourceBasePath/resources/assets/change-your-logo.svg",
-	'icon' => "$wgResourceBasePath/resources/assets/change-your-logo-icon.svg",
+	'1x' => "{{logo}}",
+	'icon' => "{{logo}}",
 ];
 
 ## UPO means: this is also a user preference option
