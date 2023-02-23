2d756259e67b7cd128b6c8aa311beb8cb50fe8b1
Wolfgang Fahl <wf@bitplan.com>
2023-02-23 17:04:37 +0100

    tries fixing tests

--- mwdocker/resources/templates/update.sh
+++ mwdocker/resources/templates/update.sh
@@ -3,3 +3,11 @@
 # WF 2021-06-22
 cd /var/www/html
 php maintenance/update.php --skip-config-validation
+# work around possible version problem - oder MediaWiki versions do no thave
+# --skip-config-validation and will signal an error
+if [ $? -ne 0 ]
+then
+  # if an error occured simply retry - no matter whether the syntax error above was
+  # the cause or any other error - no harm done except some extra time ...
+  php maintenance/update.php
+fi
