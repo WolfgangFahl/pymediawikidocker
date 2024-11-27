07d193db544f8c755183ccf31d0c74b851f6bd23
Wolfgang Fahl <wf@bitplan.com>
2024-11-27 20:21:23 +0100

    works on #79

--- mwdocker/version.py
+++ mwdocker/version.py
@@ -14,7 +14,7 @@ class Version(object):
     name = "pymediawikidocker"
     version = mwdocker.__version__
     date = "2021-06-21"
-    updated = "2024-10-26"
+    updated = "2024-11-27"
 
     authors = "Wolfgang Fahl, Tim Holzheim"
 
