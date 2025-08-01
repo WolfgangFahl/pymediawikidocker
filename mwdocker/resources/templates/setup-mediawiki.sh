227cc157d1d5cccf8c5d57b1ce60f50abedd5d6c
Wolfgang Fahl <wf@bitplan.com>
2025-08-01 13:56:41 +0200

    adss missing template

---
+++ mwdocker/resources/templates/setup-mediawiki.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# WF 2025-08-01
+# replace Dockerfile based setup
+# see https://github.com/WolfgangFahl/pymediawikidocker/issues/84
+set -e
+
+# Script directory as parameter (default fallback)
+SCRIPT_DIR="${1:-/root}"
+WEB_DIR="/var/www/html"
+
+echo "Setting up MediaWiki using scripts from: ${SCRIPT_DIR}"
+
+# Make scripts executable
+chmod +x ${SCRIPT_DIR}/*.sh
+
+# Update MediaWiki extensions via composer
+cd ${WEB_DIR}
+composer update --no-dev
+
+# Run setup scripts
+${SCRIPT_DIR}/fixPermissions.sh
+${SCRIPT_DIR}/installExtensions.sh
+
+${SCRIPT_DIR}/initdb.sh
+${SCRIPT_DIR}/addSysopUser.sh
+${SCRIPT_DIR}/addCronTabEntry.sh
+${SCRIPT_DIR}/update.sh
+
+
+echo "MediaWiki setup complete!"
